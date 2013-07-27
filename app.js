a296e6cab2b51f4815eae6b9e7c93f9c4c680d41
Evan Casey <evan.casey92@gmail.com>
2013-07-27 15:04:15 -0700

    fixed asset link in app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static('assets'));
 
 // development only
 if ('development' == app.get('env')) {
