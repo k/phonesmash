2602ec7caf0029c1f9df6a699782aa5c5f348a19
Sean McQueen <sean@mcqueen.net>
2013-07-27 13:52:14 -0700

    Grunt is set up

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        nodemon: {
+            prod: {
+                options: {
+                  file: 'app.js',
+                  ignoredFiles: ['README.md', 'node_modules/**'],
+                }
+            },
+        },
+        jshint: {
+            all: ['*.js']
+        },
+        sass: {
+            dist: {
+                files: {
+                    'public/stylesheets/styles.css': 'public/stylesheets/styles.scss'
+                }
+            }
+        }
+    });
+
+    // load tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // running tasks
+    grunt.registerTask('default', ['sass', 'jshint', 'nodemon']);
+
+};
--- app.js
+++ app.js
@@ -3,11 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
