321a26d69e3a29cc7b7c76acd6f1cf51cc1433bd
Evan Casey <evan.casey92@gmail.com>
2013-07-28 08:26:51 -0700

    asotneuh

--- assets/js/application.js
+++ assets/js/application.js
@@ -29,9 +29,9 @@ socket.on('mobileReady', function(data) {
 	}
 
 	// give the new user a panel
-	$('.player-list').append('<div class=\"panel player\" id=\'' + data.username + '\'><span id="name"><span id="time"><\/div>');
-	$('#' + data.username + ' #name').append(data.username);
-	$('#' + data.username + ' #time').append("0");
+	$('.player-list').append('<div class=\"panel player\" id=\'' + data.username + '\'><span class="name"></span><span class="time"></span><\/div>');
+	$('#' + data.username + ' .name').append(data.username);
+	$('#' + data.username + ' .time').append("0");
 
 });
 
@@ -45,35 +45,36 @@ function compare(a,b) {
 
 socket.on('started', function(data) {
 	var start = new Date(),
-			timer = setTimeout(function() {
-				$('#' + data.username).attr("#time", new Date() - start);    
+			timer = setInterval(function() {
+				console.log()
+				$('#' + data.username).find('.time').text(new Date() - start);    				
 
-			}, 100);
+			}, 10);
 
 	socket.on('elapsed', function(data) {
 		clearInterval(timer);
 
-		var currentIndex;
+		// var currentIndex;
 
-		// iterate to find the current players index
-		$.each(players, function(index, value) {		
-			if (value.name === data.name) {
-				currentIndex = index;
-			}
-		});
+		// // iterate to find the current players index
+		// $.each(players, function(index, value) {		
+		// 	if (value.name === data.name) {
+		// 		currentIndex = index;
+		// 	}
+		// });
 
-		// update the players index
-		players[currentIndex].time = data.time
+		// // update the players index
+		// players[currentIndex].time = data.time
 
-		players.sort(compare);
+		// players.sort(compare);
 
-		$('.panel .player').remove();
+		// $('.panel .player').remove();
 
-		$.each(players, function(index, value) {		
-			$('.player-list').append('<div class=\"panel player\" id=\'' + value.username + '\'><span id="name"><span id="time"><\/div>');
-			$('#' + value.username + ' #name').append(value.username);
-			$('#' + value.username + ' #time').append(value.time);
-		});
+		// $.each(players, function(index, value) {		
+		// 	$('.player-list').append('<div class=\"panel player\" id=\'' + value.username + '\'><span id="name"></span><span id="time"></span><\/div>');
+		// 	$('#' + value.username + ' #name').append(value.username);
+		// 	$('#' + value.username + ' #time').append(value.time);
+		// });
 	   
 	});
 
