5dd2102d9a63ae834ff6fc5bde0f1fd8fb666dcf
Evan Casey <evan.casey92@gmail.com>
2013-07-28 07:15:53 -0700

    sateohu

--- assets/js/application.js
+++ assets/js/application.js
@@ -24,34 +24,53 @@ socket.on('mobileReady', function(data) {
 	}
 
 	// give the new user a panel
-	$('.compete').append('<div class=\"panel player\" id=\'' + data.username + '\' data=\'' + data + '\'><\/div>');
+	$('.player-list').append('<div class=\"panel player\" id=\'' + data.username + '\'><\/div>');
 	$('#' + data.username).append(data.username);
+	$('#' + data.username).data("time", 0);
 
 });
 
-socket.on('started', function(time) {
-    // update UI
-});
+function compare(a,b) {
+  if (a.time < b.time)
+     return -1;
+  if (a.time > b.time)
+    return 1;
+  return 0;
+}
 
-socket.on('elapsed', function(data) {
-	var lowestTime = players[0].time,
-			lowestIndex, 
-			currentIndex;
 
-	// iterate to find the current players index
-	$.each(players, function(index, value) {		
-		if (value.name === data.name) {
-			currentIndex = index;
-		}
+socket.on('started', function(data) {
+	var start = new Date(),
+			timer = setTimeout(function() {
+				$('#' + data.username).data("time", new Date() - start);    
+
+			}, 100);
+
+	socket.on('elapsed', function(data) {
+		clearInterval(timer);
+
+		var currentIndex;
+
+		// iterate to find the current players index
+		$.each(players, function(index, value) {		
+			if (value.name === data.name) {
+				currentIndex = index;
+			}
+		});
+
+		// update the players index
+		players[currentIndex].time = data.time
+
+		players.sort(compare);
+
+		$('.panel .player').remove();
+
+		$.each(players, function(index, value) {		
+			$('.player-list').append('<div class=\"panel player\" id=\'' + value.username + '\' data=\'' + data + '\'><\/div>');
+			$('#' + value.username).append(value.username);
+		});
+	   
 	});
-
-	// update the players index
-	players[currentIndex].time = data.time
-	$('.compete .panel').remove();
-
-	$.each(players, function(index, value) {		
-		$('.compete').append('<div class=\"panel player\" id=\'' + value.username + '\' data=\'' + data + '\'><\/div>');
-		$('#' + value.username).append(value.username);
-	});
-   
+    
 });
+
