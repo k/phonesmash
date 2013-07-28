1638c00cc13bf0e8dba5b18568e6d361bd8ce81e
Evan Casey <evan.casey92@gmail.com>
2013-07-28 09:58:37 -0700

    aoeuth

--- assets/js/application.js
+++ assets/js/application.js
@@ -60,7 +60,7 @@ socket.on('started', function(data) {
 
 		// iterate to find the current players index
 		for(var i = 0; i < players.length; i++) {
-			if (players[i].name === data.username) {
+			if (players[i].username === data.username) {
 				players[i]['time'] = data.elapsedTime;
 			}
 		}
